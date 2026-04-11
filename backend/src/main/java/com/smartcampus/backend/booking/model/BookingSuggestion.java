d1eba48634fc516f8b6c216404c0071b9903f5d0
ChinthakaNawarathne <chinthakanawarathne99@gmail.com>
2026-04-11 10:14:55 +0530

    update bookingSuggestions.java

--- backend/src/main/java/com/smartcampus/backend/booking/model/BookingSuggestion.java
+++ backend/src/main/java/com/smartcampus/backend/booking/model/BookingSuggestion.java
@@ -3,6 +3,7 @@ package com.smartcampus.backend.booking.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+// Simple DTO to return booking suggestions when a booking request conflicts with existing bookings
 @Data
 @AllArgsConstructor
 public class BookingSuggestion {
